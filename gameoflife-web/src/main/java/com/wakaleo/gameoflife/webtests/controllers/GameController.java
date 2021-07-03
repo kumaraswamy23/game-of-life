3552291fc7e2c119a65efced02d6ff599d32dbab
Kumarswamy <kolla.kumaraswamy@gmail.com>
2021-07-03 20:42:53 +0530

    Testing222

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -31,6 +31,7 @@ public class GameController {
         Universe universe = new Universe();
         mav.addObject("universe", universe);
         System.out.println("Testing");
+        System.out.println("Testing121212");
         thinkABit(250); // Pause for random time
         return mav;
     }
